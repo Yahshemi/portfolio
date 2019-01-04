73998068d9037612d3d315cf888ebaa49cb3b37f
Yahshemi Walters <Yahshemi@gmail.com>
2019-01-03 18:57:24 -0800

    Finished the overall concept. still needs to insert links.

--- profile.js
+++ profile.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
 
 //middleware that is specific to this router
-router.use(function timeLog (req, res, next){
-    console.log('Time: ', Date.now())
-    next()
-})
+router.use(function timeLog(req, res, next) {
+  console.log("Time: ", Date.now());
+  next();
+});
 // define the home page route
-router.get('/', function(req, res) {
-    res.send('Hello world')
-})
+router.get("/", function(req, res) {
+  console.log("Hello World");
+  res.send("Hello world");
+});
 //define the about route
-router.get('/about', function (req, res){
-    res.send('About me')
-})
-module.exports = router
+router.get("/about", function(req, res) {
+  res.send("About me");
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,44 +1,48 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-var profile = require('./profile')
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var profile = require("./profile");
 const app = express();
 
-app.use(morgan("dev"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use('/profile', profile)
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use("/profile", profile);
 
 //Here we're setting the views directory to be ./views
 //thereby letting the app know where to find the template files
-app.set("views", './views');
+app.set("views", "./views");
 
 //Here we're setting the default engine to be ejs
 //note we don't need to require it, express will do that for us
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-//Now instead of using res.send we can use 
+//Now instead of using res.send we can use
 //res.render to send the output of the template by filename
-app.get('/',(req, res) => {
-    const data = {
-        person: {
-            firstName: 'Yahshemi',
-            lastName: 'Walters',
-        }
+app.get("/", (req, res) => {
+  const data = {
+    person: {
+      firstName: "Yahshemi",
+      lastName: "Walters",
+      title: "Full Stack Developer"
     }
+  };
+  // Notice now the data is the second argument passed to the template render method
+  res.render("index", data);
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
-app.get('/contact', (req, res) => {
-        res.render('contact');
-      });
-      
-      app.post('/thanks', (req, res) => {
-        res.render('thanks', { contact: req.body })
-      });
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
-    // Notice now the data is the second argument passed to the template render method
-    res.render('index', data);
+app.post("/thanks", (req, res) => {
+  res.render("thanks");
 });
 
 app.listen(8080, () => {
-    console.log('listening at http://localhost:8080');
+  console.log("listening at http://localhost:8080");
 });
